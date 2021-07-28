cd3df5b99337ca960db124ce2a5da2f42cd57e22
Ben Gregory <JBGregoryPro@protonmail.com>
2021-07-28 15:59:01 -0500

    first commit

---
+++ scripts/JewelryStyles.js
@@ -0,0 +1,40 @@
+import { getStyles, setStyle } from "./database.js"
+
+document.addEventListener(
+    "change",
+    (event) => {
+        if (event.target.name === "style") {
+            setStyle(parseInt(event.target.value))
+        }
+    }
+)
+
+
+const styles = getStyles()
+
+// document.addEventListener(
+//     "change",
+//     (event) => {
+//         if (event.target.name === "style") {
+//             window.alert(`User chose style ${event.target.value}`)
+//         }
+//     }
+// )
+
+export const JewelryStyles = () => {
+    let html = "<ul>"
+
+    // Use .map() for converting objects to <li> elements
+    const listItemsArray = styles.map(style => {
+        return `<li>
+            <input type="radio" name="style" value="${style.id}" /> ${style.style}
+        </li>`
+    })
+
+
+    html += listItemsArray.join("")
+    html += "</ul>"
+
+    return html
+}
+
